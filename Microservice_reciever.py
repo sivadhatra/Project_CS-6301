f37dd189f21cbcfdcf06b8a82d09a384827c817a
sivadhatra <karuturi.dhatra@gmail.com>
2023-05-09 15:46:27 -0500

    Update Microservice_reciever.py

--- Microservice_reciever.py
+++ Microservice_reciever.py
@@ -1,4 +1,12 @@
 import socket
+import os
+from Crypto.Cipher import AES
+from Crypto.Util.Padding import pad, unpad
+
+def decrypt(cp_text, key):
+    iv = cp_text[:AES.block_size]
+    cp = AES.new(key, AES.MODE_CBC, iv)
+    return unpad(cp.decrypt(cp_text[AES.block_size:]), AES.block_size)
 
 # Configuration
 HOST = 'localhost'
@@ -9,15 +17,38 @@ receiver_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 receiver_socket.bind((HOST, PORT))
 receiver_socket.listen(1)
 
+def generate_key():
+    try:
+        with open('key.txt', 'w') as f:
+            f.write(os.urandom(16).hex())
+    except:
+        print("Error reading file 1")
+
+def get_key():
+    try:
+        with open('key.txt', 'r') as f:
+            v = f.read()
+            #print(v)
+
+            key = bytes.fromhex(v)
+            return key
+    except:
+        print("Error reading file 2")
+
 while True:
+    generate_key()
     # Accept incoming connection
     sender_socket, sender_address = receiver_socket.accept()
 
     # Receive the encrypted data
     received_data = sender_socket.recv(1024)
-    received_data=received_data.decode()
+    #print(received_data)
+    #received_data=received_data.decode()
+    received_data=decrypt(received_data,get_key())
     # Print the decrypted data
-    print("Received data:", received_data)
+    #print("Received data:", received_data)
+    decrypted_plaintext = received_data.decode()
+    print("Received decrypted data:", decrypted_plaintext)
 
     # Close the connection
     sender_socket.close()
